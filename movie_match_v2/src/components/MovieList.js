29f4798e1a56f6c655ff8e53fa153e989fb91b13
jonathan <jtmacoco@mail.csuchico.edu>
2023-08-07 17:51:42 -0700

    got remove movie functionality working

--- movie_match_v2/src/components/MovieList.js
+++ movie_match_v2/src/components/MovieList.js
@@ -8,7 +8,8 @@ import { useAuth } from '../context/AuthContext';
 import { IoIosAddCircle } from "react-icons/io";
 import Navbar from './Navbar';
 import { BsFillCheckCircleFill } from "react-icons/bs";
-import { collection, doc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { AiFillMinusCircle } from "react-icons/ai";
+import { collection, getDoc, doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 export default function MovieList() {
     const [data, setData] = useState([])
     const { theme, toggleTheme } = useTheme();
@@ -21,13 +22,17 @@ export default function MovieList() {
     const [hoverState, setHoverState] = useState({});
     const [documentId, setDocumentId] = useState('');
     const [addMovie, setAddMovie] = useState({});
-    const toggleHover = (movieId) => {
+    const [removeMovie, setRemoveMovie] = useState({});
+    const toggleHover = (listId, movieId) => {
         setHoverState((prev) => ({
             ...prev,
-            [movieId]: !prev[movieId],
+            [listId]: {
+                ...prev[listId],
+                [movieId]: !prev[listId]?.[movieId] || false,
+            },
+        }));
+    };
 
-        }))
-    }
     const fetchData = async () => {
         const res = await userData()
         const filterData = res.filter(item => item.uid === currentUser.uid)
@@ -38,11 +43,22 @@ export default function MovieList() {
     }
     const updateMovieList = async () => {
         try {
-            console.log( addMovie );
+            console.log(addMovie);
             const docRef = doc(db, 'userData', documentId[0]);
             await updateDoc(docRef, {
                 movieList: arrayUnion(addMovie),
             });
+            setData((prevData) =>
+                prevData.map((item) => {
+                    if (item.id === documentId[0]) {
+                        return {
+                            ...item,
+                            movieList: [...item.movieList, addMovie],
+                        };
+                    }
+                    return item;
+                })
+            );
             console.log('movie added');
         } catch (error) {
             console.log(documentId)
@@ -51,6 +67,33 @@ export default function MovieList() {
         }
 
     }
+    const handleRemove = async () => {
+        try {
+            const docRef = doc(db, 'userData', documentId[0]);
+            const docSnap = await getDoc(docRef);
+    
+            if (docSnap.exists()) {
+                const docData = docSnap.data();
+                
+                // Assuming 'movieList' is a nested array field
+                console.log("remove id, ", removeMovie[0].id);
+                const updatedMovieList = docData.movieList.filter(movie => movie.id !== removeMovie[0].id);
+
+                console.log("trying to print ", updatedMovieList.map((info) => info.title));
+                
+                // Update the document with the modified data
+                await updateDoc(docRef, { movieList: updatedMovieList });
+    
+                console.log('movie has been removed');
+            } else {
+                console.log('Document not found');
+            }
+        } catch (error) {
+            console.error('Error removing movie: ', error);
+        }
+    };
+    
+    
     const options = {
         method: 'GET',
         headers: {
@@ -74,12 +117,30 @@ export default function MovieList() {
             console.log("Movie not found.");
         }
     };
+    const getRemoveMovie = (movieId) =>{
+        //const movie = data.movieList.find((value) => value.id === movieId);
+//        const movie = data.find((value) => value.id === movieId);
+        const movie = data.map((info)=>info.movieList.find((value) => value.id === movieId)); 
+        console.log("movie: ", movie);
+        if (movie) {
+            setRemoveMovie(movie);
+            console.log("found movie : ",movie)
+        } else {
+            console.log("Movie not found.");
+        }
+    }
     useEffect(() => {
         if (addMovie) {
-            updateMovieList() 
-          console.log("Found movie:", addMovie.title); // This will show the correct movie title.
+            updateMovieList()
+            console.log("Found movie:", addMovie.title); 
         }
-      }, [addMovie]);
+    }, [addMovie]);
+    useEffect(()=>{
+        if(removeMovie){
+            handleRemove()
+            console.log("Found movie:", removeMovie.title); 
+        }
+    },[removeMovie])
     useEffect(() => {
         fetchData()
     }, [])
@@ -112,7 +173,7 @@ export default function MovieList() {
     const displayAdd = (movieId) => {
         if (seenMovies.length === 0) {
             return (<>
-                <button onClick={() => { getAddMovie(movieId);   }}>
+                <button onClick={() => { getAddMovie(movieId); }}>
                     <p>add</p>
                     <div className='absolute top-1 right-1/3 '>
                         <IoIosAddCircle color="white">add</IoIosAddCircle>
@@ -120,30 +181,30 @@ export default function MovieList() {
                 </button>
             </>)
         }
-        return seenMovies.map(seen => {
-            if (seen.id === movieId) {
-                return (<><p>added</p>
+        const movieFound = seenMovies.find(seen => seen.id === movieId);
+
+        if (movieFound) {
+            return (
+                <>
+                    <p>added</p>
                     <div className='absolute top-1 right-1/3 '>
                         <BsFillCheckCircleFill color="green" />
                     </div>
                 </>
-                )
-            }
-            else {
-                return (
-                    <>
-                        <button onClick={() => { getAddMovie(movieId);}}>
-                            <p>add</p>
-                            <div className='absolute top-1 right-1/3 '>
-                                <IoIosAddCircle color="white">add</IoIosAddCircle>
-                            </div>
-                        </button>
-                    </>
+            );
+        } else {
+            return (
+                <>
+                    <button onClick={() => { getAddMovie(movieId); }}>
+                        <p>Add</p>
+                        <div className='absolute top-1 right-1/3 '>
+                            <IoIosAddCircle color="white">add</IoIosAddCircle>
+                        </div>
+                    </button>
+                </>
+            );
+        }
 
-                )
-
-            }
-        })
     }
     const handleKeyUp = (e) => {
         if (e.key === "Enter") {
@@ -170,24 +231,40 @@ export default function MovieList() {
                 <motion.div animate={{ x: theme === "dark" ? 5 : 40 }}>{Icon}</motion.div>
             </button>
             <div className='flex justify-center items-center'>
-                <h1 className="absolute top-10 text-center text-5xl pb-14 font-movieMatch text-black dark:text-white">Your Movie List</h1>
+                <h1 className="absolute top-5 text-center text-5xl pb-14 font-movieMatch text-black dark:text-white">Your Movie List</h1>
             </div>
-            <div className='relative top-28  flex justify-center'>
-                <div className='flex  pb-[100px] flex-cols gap-4'>
+            <div className='relative top-24  flex justify-center '>
+                <div className='pl-10 flex  pt-4 pb-10 flex-cols gap-4 overflow-x-auto scroll-smooth'>
                     {data.map(info => (
                         info.movieList.map(movieInfo => (
-                            <motion.div whileHover={{ scale: 1.2 }}>
-                                <img
-                                    key={movieInfo.title}
-                                    className="w-fit h-96 lg:h-80 xl:h-80 2xl:h-96 border border-neutral-500 rounded-xl"
-                                    src={`https://image.tmdb.org/t/p/original${movieInfo.poster_path}`}
-                                />
-                            </motion.div>
+                            <div className='pt-4 div-flex flex-none '>
+                                <motion.div whileHover={{ scale: 1.2 }}>
+                                    <img
+                                        onMouseEnter={() => toggleHover(info.id, movieInfo.id)}
+                                        onMouseLeave={() => toggleHover(info.id, movieInfo.id)}
+                                        key={movieInfo.title}
+                                        className="w-fit h-96 lg:h-80 xl:h-80 2xl:h-96 border border-neutral-500 rounded-xl"
+                                        src={`https://image.tmdb.org/t/p/original${movieInfo.poster_path}`}
+                                    />
+                                    <div
+                                        onMouseEnter={() => toggleHover(info.id, movieInfo.id)}
+                                        onMouseLeave={() => toggleHover(info.id, movieInfo.id)}
+                                        className={`${hoverState[info.id]?.[movieInfo.id] ? "block" : "hidden"
+                                            } opacity-80 text-white text-center absolute  bg-black bottom-0  w-[214px] h-20 rounded-lg`} >
+                                        <button onClick={()=>{ getRemoveMovie(movieInfo.id)}}>
+                                            <p>Remove</p>
+                                            <div className='absolute top-1 right-1/4 '>
+                                                <AiFillMinusCircle color="white" />
+                                            </div>
+                                        </button>
+                                    </div>
+                                </motion.div>
+                            </div>
                         ))
                     ))}
                 </div>
             </div>
-            <div className='absolute bottom-1/3 items-center justify-center w-full'>
+            <div className='absolute bottom-1/4 items-center justify-center w-full'>
                 <div className=' flex items-center justify-center w-full'>
                     <div>
                         <input
@@ -214,17 +291,17 @@ export default function MovieList() {
                             <div key={index} className="py-12 d-flex flex-none flex flex-cols">
                                 <motion.div whileHover={{ scale: 1.2 }}>
                                     <img
-                                        onMouseEnter={() => toggleHover(movieInfo.id)}
-                                        onMouseLeave={() => toggleHover(movieInfo.id)}
+                                        onMouseEnter={() => toggleHover(movieData.id, movieInfo.id)}
+                                        onMouseLeave={() => toggleHover(movieData.id, movieInfo.id)}
                                         className="w-fit h-96 px-1 rounded-2xl "
                                         src={`https://image.tmdb.org/t/p/original${movieInfo.poster_path}`}
                                         alt={`Movie Poster ${index}`}
                                     />
                                     <div className='pl-1'>
                                         <div
-                                            onMouseEnter={() => toggleHover(movieInfo.id)}
-                                            onMouseLeave={() => toggleHover(movieInfo.id)}
-                                            className={`${hoverState[movieInfo.id] ? "block" : "hidden"
+                                            onMouseEnter={() => toggleHover(movieData.id, movieInfo.id)}
+                                            onMouseLeave={() => toggleHover(movieData.id, movieInfo.id)}
+                                            className={`${hoverState[movieData.id]?.[movieInfo.id] ? "block" : "hidden"
                                                 } opacity-80 text-white text-center absolute  bg-black bottom-0  w-[256px] h-20 rounded-lg`} >
 
 
