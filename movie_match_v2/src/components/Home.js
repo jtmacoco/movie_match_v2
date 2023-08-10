246b88aac2b1cc3cda0ea678314e390ffab3fac0
jonathan <jtmacoco@mail.csuchico.edu>
2023-08-10 14:46:09 -0700

    added info button to home page

--- movie_match_v2/src/components/Home.js
+++ movie_match_v2/src/components/Home.js
@@ -7,12 +7,14 @@ import { useAuth } from "../context/AuthContext";
 import Navbar from "./Navbar";
 import { userData } from '../userData'
 import { matchList } from "../matchesList";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 export default function Home() {
   const { theme, toggleTheme } = useTheme();
   const { Icon, toggleIcon } = useIcon();
   const { currentUser } = useAuth()
   const [data, setData] = useState([])
   const [matches, setMatches] = useState([])
+  const [info, setInfo] = useState(false);
   const fetchData = async () => {
     const res = await userData()
     const filterData = res.filter(item => item.uid === currentUser.uid)
@@ -34,6 +36,7 @@ export default function Home() {
       document.documentElement.classList.remove("dark");
     }
   }, [theme]);
+
   return (
     <div className={`${theme === "dark" ? "bg-dark_back" : "bg-light_back"} h-screen flex justify-center items-center flex-col`}>
       <button
@@ -44,15 +47,33 @@ export default function Home() {
           toggleIcon();
         }}
       >
+
         <motion.div animate={{ x: theme === "dark" ? 5 : 40 }}>{Icon}</motion.div>
       </button>
+      <div className={`${info ? "visible": "hidden"} absolute z-40 top-2`}>
+        <div class="w-96 rounded-md flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3" role="alert">
+          <div className="absolute top-2 pr-2">
+            <BsFillInfoCircleFill size={20} />
+          </div>
+          <div className="pt-6">
+            Users who are displayed at the top of the list have a similar movie taste to your own. Those who are further down the list will have less of a similar movie interest.
+            <button onClick={()=>setInfo(false)} className="absolute right-3">close</button>
+          </div>
+
+        </div>
+      </div>
       {data.map(user => (
         <h1 className="dark:text-white absolute top-0 left-20 font-bold">
           Welcome {user.username}
         </h1>
       ))}
-      <div className="absolute top-10">
+      <div className="absolute top-10 flex flex-cols">
         <h1 className="text-center text-5xl pb-14 font-movieMatch text-black dark:text-white">New Users to Chat With</h1>
+        <div className="pl-2">
+          <button onClick={()=>setInfo(true)}>
+            <BsFillInfoCircleFill size={20} />
+          </button>
+        </div>
       </div>
       <div className="absolute gap-y-4 flex items-center flex-col top-40  ">
         {matches.map(userData => (
