9712f02ce1103cb4a82269f6c5a9332efd1fb121
jonathan <jtmacoco@mail.csuchico.edu>
2023-08-14 17:00:38 -0700

    fixed chat formating

--- movie_match_v2/src/components/ChatPage.js
+++ movie_match_v2/src/components/ChatPage.js
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { useIcon } from "../context/IconContext";
 import { AnimatePresence, motion } from "framer-motion";
@@ -23,11 +23,11 @@ const ChatPage = (userId) => {
     const { currentUser } = useAuth()
     const [curMess, setCurMess] = useState({})
     const [displayMessage, setDisplayMessage] = useState([])
+    const messegeRef = useRef();
     const fetchMessages = async (docId) => {
         try {
-            console.log("docId: ", docId);
             const messagesRef = collection(db, "messages", docId, "texts")
-            const q = query(messagesRef, orderBy('time'), limit(25));
+            const q = query(messagesRef, orderBy('time'));
             const querySnapshot = await getDocs(q);
             const messages = []
             querySnapshot.forEach((doc) => {
@@ -77,32 +77,49 @@ const ChatPage = (userId) => {
             console.log("error adding texts: ", error)
         }
     }
+    useEffect(() => {
+        // Scroll to the bottom of the messages when the component updates
+        console.log("useEffect triggered");
+        const element = messegeRef.current;
+        const scrollOptions = {
+            top: element.scrollHeight,
+            behavior: 'smooth', // This enables smooth scrolling
+        };
+    
+        element.scrollTo(scrollOptions);
+    }, [displayMessage]);
     function ChatMessage(props) {
         const { text, sender_Id } = props.message;
         const curSender = sender_Id === currentUser.uid;
-    
+
         // Determine the appropriate alignment class based on the sender
-        const alignmentClass = curSender ? "justify-end" : "justify-start";
-    
+        const alignmentClass = curSender ? "self-end" : "self-start";
+        const curS = curSender ? "relative right-0 " : "relative left-0";
+        const start_end = curSender ? "chat-end" : "chat-start"
         return (
-            <div className={`flex  ${alignmentClass} mb-2`}>
-                <div className={`rounded-lg p-2 ${curSender ? "bg-blue-300" : "bg-gray-300"}`}>
-                    {text}
+            <div className={`flex pb-2 ${alignmentClass}`}>
+            <div  className={` chat ${start_end} ${curS}   `}>
+                <div className={` chat-bubble  ${curSender ? "bg-blue-500" : "bg-gray-600"}`}>
+                    <p className=' pr-2 text-white text-md'> {text}</p>
                 </div>
             </div>
+</div>
         );
     }
-    
+
     return (
         <div className={`${theme === "dark" ? "bg-dark_back" : "bg-light_back"} h-screen flex justify-center items-center flex-col`}>
             <ThemeToggle />
-            <Navbar />
             <h1 className="absolute top-10 text-center text-5xl pb-14 font-movieMatch text-black dark:text-white">Chat with</h1>
-            {displayMessage.map((m) => (
-                <div className='flex flex-rows'>
-                    <ChatMessage message={m} />
+            <section className='absolute top-10 w-screen bottom-24 overflow-y-auto'>
+                <div   ref={messegeRef} className='flex flex-col  rounded-lg' style={{ maxHeight: 'calc(100vh - 20vh)' }}>
+                    {displayMessage.map((m, index) => (
+                            <ChatMessage key={index} message={m} />
+                    ))}
                 </div>
-            ))}
+            
+            </section>
+           
             <div className='absolute bottom-0'>
                 <form onSubmit={handleSubmit} id="textForm" >
                     <textarea
@@ -114,7 +131,10 @@ const ChatPage = (userId) => {
                     <button type="submit" className='h-20  text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 absolute bottom-0 right-0'>send</button>
                 </form>
             </div>
+
+            <Navbar />
         </div>
+
     )
 
 };
