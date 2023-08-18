f494c209a6034abd0b71209c5d249da0388c77a2
jonathan <jtmacoco@mail.csuchico.edu>
2023-08-18 16:42:29 -0700

    website is fully working need to clean up code

--- movie_match_v2/src/deleteMessages.js
+++ movie_match_v2/src/deleteMessages.js
@@ -3,22 +3,28 @@ import { useAuth } from './context/AuthContext';
 import { db } from './firebase';
 import { doc, collection, deleteDoc, getDocs } from "firebase/firestore";
 
-export const deleteMessages = async(curUid, userUid) =>{
+export const deleteMessages = async (curUid, userUid) => {
     console.log("hi")
-    const messagesQuerySnapshot = await getDocs(collection(db,"messages"));
-//    const messagesCollectionRef = collection(db, "messages");
+    const messagesQuerySnapshot = await getDocs(collection(db, "messages"));
+    //    const messagesCollectionRef = collection(db, "messages");
     for (const messageDoc of messagesQuerySnapshot.docs) {
         const messageData = messageDoc.data();
-        if((messageData.user1_Id === userUid && messageData.user2_Id === curUid)||
-           messageData.user2_Id === userUid && messageData.user1_Id === curUid
-        )
-        {
-            try{
-                await deleteDoc(doc(db,'messages',messageDoc.id))
+        if ((messageData.user1_Id === userUid && messageData.user2_Id === curUid) ||
+            messageData.user2_Id === userUid && messageData.user1_Id === curUid
+        ) {
+            const messageSubcollectionRef = collection(messageDoc.ref, "texts");
+            const subCollectionSnapshot = await getDocs(messageSubcollectionRef);
+            try {
+                for (const textDoc of subCollectionSnapshot.docs) {
+                    await deleteDoc(textDoc.ref);
+                    console.log("delete sub collection")
+                }
+                await deleteDoc(doc(db, 'messages', messageDoc.id))
                 console.log("delted message dat successfully")
-            }catch(error){
+            } catch (error) {
                 console.log("error: ", error)
             }
         }
+
     }
 }
--- movie_match_v2/src/deleteUserData.js
+++ movie_match_v2/src/deleteUserData.js
@@ -3,6 +3,7 @@ import { auth, db } from "./firebase";
 import { deleteDoc,getDoc,doc,collection, getDocs } from "firebase/firestore";
 
 export const deleteUserData= async(uid) =>{
+    console.log("delete user data")
     const messagesQuerySnapshot = await getDocs(collection(db,"messages"));
     const userDataQuerySnapshot = await getDocs(collection(db,"userData"));
 //    const messagesCollectionRef = collection(db, "messages");
@@ -10,7 +11,13 @@ export const deleteUserData= async(uid) =>{
         const messageData = messageDoc.data();
         if(messageData.user1_Id === uid || messageData.user2_Id === uid)
         {
+            const messageSubcollectionRef = collection(messageDoc.ref, "texts");
+            const subCollectionSnapshot = await getDocs(messageSubcollectionRef);
             try{
+                for (const textDoc of subCollectionSnapshot.docs) {
+                    await deleteDoc(textDoc.ref);
+                    console.log("delete sub collection")
+                }
                 await deleteDoc(doc(db,'messages',messageDoc.id))
                 console.log("delted message data successfully")
             }catch(error){
@@ -23,6 +30,7 @@ export const deleteUserData= async(uid) =>{
         const userData = userDataDoc.data();
         if(userData.uid === uid)
         {
+            
             try{
                 await deleteDoc(doc(db,'userData',userDataDoc.id))
                 console.log("delted user data successfully")
